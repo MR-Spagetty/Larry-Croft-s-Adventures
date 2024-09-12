735f89944fc2af24a39973f0101b1bf0de9bf9a2
Developer 1 <dev1@example.internal>
2024-09-13 11:28:06 +1200

    Almost finished full version of KeyStrokes.java
    Also made minor changes to UML Diagram. About to start work on start screen.

--- src/nz/ac/wgtn/swen225/lc/App/KeyStrokes.java
+++ src/nz/ac/wgtn/swen225/lc/App/KeyStrokes.java
@@ -9,54 +9,43 @@ import java.util.ArrayDeque;
  * Stores all of the keystrokes that associate with an action that is executed (or a direction that is set)
  * when a key is pressed.
  */
+class KeyStrokes <V extends Direction, Runnable> {
+    V active = null;
 
-// TODO make the direction emus
-class KeyStrokes {
-    Direction active = null;
-
-    private final <Integer, Direction> strokeToDirection = new HashMap<>();
-    private final <Integer, Runnable> strokeToRunnable = new HashMap<>();
+    private final <Integer, V> strokes = new HashMap<>();
     private Queue<Integer> pendingKeyStrokes = new ArrayDeque<>();
 
     /**
-     * Binds a keycode to a direction in which the character can move.
+     * Binds a keycode to a direction in which the character can move, or an action that can be initiated on the
+     * game's UI during gameplay.
      *
-     * @param keyCode The keycode of the key that will be associated with the direction.
-     * @param direction The direction in which the character will move.
+     * @param keyCode The keycode of the key
+     * @param elem A direction or an action that will be assiocated with the key.
      */
-    public void assignKeyCode(int keyCode, Direction direction){ strokeToDirection.put(keyCode, direction); }
-
-    /**
-     * Binds a keycode to an action that is performed on the game's UI during gameplay.
-     *
-     * @param keyCode The keycode of the key that will be associated with the action.
-     * @param action The action that will be performed when the key(s) are pressed.
-     */
-    public void assignKeyCode(int keyCode, Runnable action){ strokeToRunnable.put(keyCode, action); }
-
-    /**
-     * Gets the direction or the action that is associated with the given key code.
-     *
-     * @param keyCode The keycode of the key that will be associated with the action.
-     * @return
-     */
-    public <V> V getKeyStroke(int keyCode){
-        Direction d = strokeToDirection.get(keyCode);
-        Runnable r = strokeToRunnable.get(keyCode);
-
-        /** are assertions enabled? */
-        assert (d != null) || (r != null);
-
-        if (d != null) return d;
-        if (r != null) return r;
+    public void assignKeyStroke(int keyCode, V elem){
+        strokes.put(keyCode, elem);
     }
 
     /**
-     * todo
+     * Gets the direction or the action that is associated with the given key code.
+     * The return value cannot be null.
+     *
+     * @param keyCode The keycode of the key that will be associated with the action.
+     * @return A direction or an action associcated with the keycode.
+     */
+    public V getKeyStroke(int keyCode){
+        V returnValue = strokes.get(keyCode);
+        assert returnValue != null; /** are assertions enabled? */
+        return returnValue;
+    }
+
+    /**
+     * Every time a tick occurs, the action that is being performed or the direction in which the
+     * character is moving stops moving, and the next action/direction is selected.
      */
     public void tick(){
         active = null;
-        if (!pendingKeyStrokes.isEmpty()) active = getDirection(pendingKeyStrokes.poll())
+        if (!pendingKeyStrokes.isEmpty()) active = getKeyStroke(pendingKeyStrokes.poll())
         pendingKeyStrokes.clear();
     }
 }
