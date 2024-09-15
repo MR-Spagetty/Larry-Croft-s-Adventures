144f4870b6087e6b62580aa43176f1a3148011b0
Developer 1 <dev1@example.internal>
2024-09-15 21:35:47 +1200

    Made a "rough" version of the Start Menu

--- src/main/nz/ac/wgtn/swen225/lc/App/StartScreen.java
+++ src/main/nz/ac/wgtn/swen225/lc/App/StartScreen.java
@@ -1,18 +1,21 @@
 package nz.ac.wgtn.swen225.lc.App;
 
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+/**
+ * Class which controls the "Start Menu" GUI, including the Buttons and their corresponding actions.
+ */
 public class StartScreen extends JFrame{
-    /** The below code is dependent on whether a new game is being made from a .JSON file. */
-    Runnable createNewGame; //Program to execute when a new game is created.
-    Runnable loadExistingGame; //Program
+    Runnable closeGame= ()->{}; //Action to be executed when the user closes the Game GUI with the 'X' button.
 
-    Runnable closeGame= ()->{};
-
+    /**
+     * Constructor for the "Start Menu", which involves setting up the frame and the buttons that will be inside the
+     * frame. It also makes sure that the JFrame is disposed when the user closes the window, and that the game will
+     * be running on the "Event Dispatch" thread.
+     */
     public StartScreen(){
         assert SwingUtilities.isEventDispatchThread();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -28,13 +31,42 @@ public class StartScreen extends JFrame{
         setVisible(true);
     }
 
-    public void createStartMenu(){
-        JTextArea instructions = new JTextArea("Instructions go here!");
-        JButton start = new JButton("Start the Game!");
-        JButton load = new JButton("Load existing game!");
+    /**
+     * Helper method that creates the "components" that will be in the Game GUI.
+     */
+    private void createStartMenu(){
+        add(BorderLayout.NORTH, new JLabel("Instructions go here!"));
+        add(BorderLayout.CENTER, createButtonsSection());
     }
 
-    public void runGame(){
-        /** todo */
+    /**
+     * Creates a "JPanel" that will hold the buttons of the Start Menu. One of the buttons will start the game
+     * itself, and if a game has not been loaded (which is loaded by pressing the "Load" button), a new game will
+     * be initiated.
+     */
+    private JPanel createButtonsSection(){
+        JPanel buttons = new JPanel();
+        JButton start = new JButton("Start the Game!");
+        JButton load = new JButton("Load existing game!");
+
+        buttons.add(start);
+        buttons.add(load);
+
+        start.addActionListener(unused -> runGame());
+        start.addActionListener(unused -> loadGame());
+
+        return buttons;
+    }
+
+    private void runGame(){
+        /** todo: Create new game file and then run game, if specified game file is blank! */
+        System.out.println("Test");
+    }
+
+    /**
+     * Loads an existing game from a ".json" file.
+     */
+    private void loadGame(){
+        /** todo: If neccesary, create a file chooser for selecting a game file. */
     }
 }
