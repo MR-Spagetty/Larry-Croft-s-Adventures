47c2aeec334f67dab73825ea2b5cb54c8066055c
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2024-09-17 19:16:48 +1200

    Fixed move and added UID in Entity
    Also added setMaze method

--- src/main/nz/ac/wgtn/swen225/lc/domain/Entity.java
+++ src/main/nz/ac/wgtn/swen225/lc/domain/Entity.java
@@ -11,6 +11,15 @@ public interface Entity {
   // TODO extend whatever JSONable interface is created for persistency
 
   /**
+   * Gets the unique id of the entity for use in identification of the entity
+   *
+   * <p>primarily designed for use in randomisation of actions
+   *
+   * @return the unique id of this entity
+   */
+  long getUID();
+
+  /**
    * Returns the timestamp of the last game tick when this entity was updated.
    *
    * @return the timestamp of the last game tick
@@ -40,20 +49,38 @@ public interface Entity {
    * @param in the maze in which to move this entity
    */
   default void move(Point by) {
+    Tile newLoc =
+        getMaze()
+            .getTile(getLocation().add(by))
+            .orElseThrow(
+                () -> new IllegalArgumentException("requested tile does not exist in maze"));
     Tile oldLoc =
-        getMaze().getTile(getLocation())
+        getMaze()
+            .getTile(getLocation())
             .orElseThrow(
                 () ->
                     new IllegalArgumentException(
-                        "Given maze does not contain tile this entity occupies"));
-    Tile newLoc =
-        getMaze().getTile(getLocation().add(by))
-            .orElseThrow(
-                () -> new IllegalArgumentException("requested tile does not exist in maze"));
-    newLoc.enter(this);
-    setLocation(getLocation().add(by));
+                        "Maze does not contain tile this entity occupies"));
+    if (!newLoc.canEnter(this)) {
+      throw new IllegalArgumentException("Entity may not enter the requested tile");
+    }
     oldLoc.leave(this);
+    newLoc.enter(this);
   }
 
+  /**
+   * gets the maze that this Entity inhabits
+   *
+   * @return the inhabited maze
+   */
   Maze getMaze();
+
+  /**
+   * sets the maze that this Entity inhabits
+   *
+   * <p>Should be only useable once
+   *
+   * @param maze the maze to inhabit
+   */
+  void setMaze(Maze maze);
 }
