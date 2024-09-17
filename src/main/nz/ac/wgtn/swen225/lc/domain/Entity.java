b2d0151bf7f057bd063bd82f4a40f2c53a7ee445
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2024-09-17 18:31:26 +1200

    Updated Entity and Tile interfaces
    added more required methods to entity and tile interfaces
    
    updated Entity more method to not take in maze as that is something the entity should know about

--- src/main/nz/ac/wgtn/swen225/lc/domain/Entity.java
+++ src/main/nz/ac/wgtn/swen225/lc/domain/Entity.java
@@ -1,10 +1,10 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-
 /**
  * Basic interface that all entities in the game must implement.
  *
  * <p>This interface provides methods for managing entities' state, location, and game ticks.
+ *
  * @author MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
  */
 public interface Entity {
@@ -31,11 +31,27 @@ public interface Entity {
    */
   Point getLocation();
 
+  void setLocation(Point newLocation);
+
   /**
    * Moves this entity by the specified amount in the given maze.
    *
    * @param by the amount to move this entity
    * @param in the maze in which to move this entity
    */
-  void move(Point by, Maze in);
+  default void move(Point by, Maze in) {
+    Tile oldLoc =
+        in.getTile(getLocation())
+            .orElseThrow(
+                () ->
+                    new IllegalArgumentException(
+                        "Given maze does not contain tile this entity occupies"));
+    Tile newLoc =
+        in.getTile(getLocation().add(by))
+            .orElseThrow(
+                () -> new IllegalArgumentException("requested tile does not exist in maze"));
+    newLoc.enter(this);
+    setLocation(getLocation().add(by));
+    oldLoc.leave(this);
+  }
 }
