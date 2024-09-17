8769fb1cb14239b4a4cc7ac1d491d548d12cc78d
Developer 4 <dev4@example.internal>
2024-09-17 12:44:45 +1200

    Finished skelston for the recorder class

---
+++ src/main/nz/ac/wgtn/swen225/lc/App/Main.java
@@ -0,0 +1,18 @@
+package nz.ac.wgtn.swen225.lc.App;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    /**
+     * Method which starts up the "Chip's Challenge" game.
+     *
+     * @author Developer 1 <dev1@example.internal>
+     * @version 1.0
+     * @param args An Array of arguments that are passed through when the program is initiated.
+     *             This is not used!
+     */
+    public static void main(String[] args){
+        System.out.println("Loading Game!");
+        SwingUtilities.invokeLater(StartScreen::new);
+    }
+}
