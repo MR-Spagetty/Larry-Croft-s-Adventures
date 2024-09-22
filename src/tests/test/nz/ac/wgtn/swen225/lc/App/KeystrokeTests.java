7657edb0a4ca6e1b376706adc7ed6c789544c3d7
Developer 1 <dev1@example.internal>
2024-09-22 13:29:56 +1200

    Made two invalid tests plus an Assertions enabled test

--- src/tests/test/nz/ac/wgtn/swen225/lc/App/KeystrokeTests.java
+++ src/tests/test/nz/ac/wgtn/swen225/lc/App/KeystrokeTests.java
@@ -1,11 +1,27 @@
+package test.nz.ac.wgtn.swen225.lc.App;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import nz.ac.wgtn.swen225.lc.App.ControlKeys;
 
 public class KeystrokeTests{
     ControlKeys keys = new ControlKeys();
+    Class<IllegalArgumentException> iae = IllegalArgumentException.class;
+    Class<NullPointerException> npe = NullPointerException.class;
+    Class<IllegalCallerException> ice = IllegalCallerException.class;
+
+    @Test void assertionsEnabled(){
+        Assertions.assertThrows(AssertionError.class, () -> { assert false; });
+    }
 
     @Test void strokeDoesntMapAnywhere(){
-        keys.keyPressed();
+        keys.setNextKeyStroke(KeyEvent.VK_G); //"G" is not assigned to a Player action in the game.
+        Assertions.assertThrows(npe, setPlayerActionAtTick());
+    }
+
+    @Test void strokeMapsToUIAction(){
+        keys.setNextKeyStroke(KeyEvent.VK_1); //"1" is mapped to a Runnable action, not a Player action.
+        Assertions.assertThrows(ice, setPlayerActionAtTick());
     }
 }
