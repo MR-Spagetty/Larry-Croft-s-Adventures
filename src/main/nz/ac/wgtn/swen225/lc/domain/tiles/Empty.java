415fba16c107d8c74ee3026461b771a63995ec8c
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2024-09-25 23:31:33 +1200

    Created Empty Tile

---
+++ src/main/nz/ac/wgtn/swen225/lc/domain/tiles/Empty.java
@@ -0,0 +1,48 @@
+package nz.ac.wgtn.swen225.lc.domain.tiles;
+
+import java.util.Optional;
+import nz.ac.wgtn.swen225.lc.domain.Entity;
+import nz.ac.wgtn.swen225.lc.domain.Point;
+
+public class Empty implements Tile {
+
+  /**
+   * creates a new Empty tile at the given location
+   *
+   * @param location the location to create the tile at
+   */
+  public Empty(Point location) {
+    this.location = location;
+  }
+
+  private Optional<Entity> occupant = Optional.empty();
+  private final Point location;
+
+  @Override
+  public Point location() {
+    return location;
+  }
+
+  @Override
+  public boolean canEnter(Entity enteree) {
+    // TODO checks with advanced occupants
+    return getOccupant().isEmpty();
+  }
+
+  @Override
+  public void enter(Entity enteree) {
+    // TODO handeling of advanced occupants
+    if (this.occupant.isPresent()) {
+      throw new IllegalStateException(
+          "The entity: %l may not enter this tile".formatted(enteree.getUID()));
+    }
+    this.occupant = Optional.of(enteree);
+  }
+
+  @Override
+  public void leave(Entity exitee) {
+    if (this.occupant.map(e -> e.equals(exitee)).orElse(false)) {
+      this.occupant = Optional.empty();
+    }
+  }
+}
