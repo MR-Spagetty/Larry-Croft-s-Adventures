ac2a98601a50cb6630836f661024b0533f73fb65
Developer 5 <dev5@example.internal>
2024-09-26 15:04:26 +1200

    Copied JSONLong to create JSONDouble

---
+++ src/main/nz/ac/wgtn/swen225/lc/persistency/JSONDouble.java
@@ -0,0 +1,27 @@
+package nz.ac.wgtn.swen225.lc.persistency;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// This allows selective serialization of key-value pairs.
+// For e.g. only certain fields of a Player object might need to be saved to JSON.
+
+public class JSONDouble implements JSONType {
+  private static Map<String, Double> dataForObject = new HashMap<>();
+  // Strings, booleans, int, longs etc.
+  public void add(String key, Double value) {
+    dataForObject.put(key, value);
+  }
+
+  @Override
+  public String toJson() {
+    // Convert the map to JSON using a library like Gson, or manually
+    return new Gson().toJson(dataForObject);
+  }
+
+  public Object get(String x) {
+    return x;
+  }
+}
