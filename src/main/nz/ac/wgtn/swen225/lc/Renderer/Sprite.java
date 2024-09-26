65606a795c79cc9bee1a98fb9cd33a715bb4bf5e
Developer 2 <dev2@example.internal>
2024-09-27 00:02:47 +1200

    Remade constructors, draw method complete

--- src/main/nz/ac/wgtn/swen225/lc/Renderer/Sprite.java
+++ src/main/nz/ac/wgtn/swen225/lc/Renderer/Sprite.java
@@ -1,62 +1,82 @@
 package nz.ac.wgtn.swen225.lc.Renderer;
-import nz.ac.wgtn.swen225.lc.domain.tiles.*;
-import nz.ac.wgtn.swen225.lc.domain.Point;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.imageio.ImageIO;
+
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import nz.ac.wgtn.swen225.lc.domain.Point;
+import nz.ac.wgtn.swen225.lc.domain.entities.*;
+import nz.ac.wgtn.swen225.lc.domain.tiles.*;
 
+/** 
+ * Stores graphical image data, as well as position. 
+ * 
+ * Can use stored data to render onto screen.
+*/
 public class Sprite {
-    static Graphics graphics;
-    BufferedImage image;
-    int priority;
-    Point position;
-    
-    Sprite(Tile tile) {
-        this.image = resolveImage(tile);
-        this.priority = resolvePriority(tile);
-        this.position = tile.location();
-    }
+  private static Graphics graphics;
+  private BufferedImage image;
+  private Point position;
 
-    /**
-    * 
-    * @param tile an object that implements Tile
-    * @return the image corresponding to the type of tile
-    */
-    static BufferedImage resolveImage(Tile tile) {
-        // try-catch for file handling exceptions
-        try{
-            return switch (tile) {
-                case Empty empty -> ImageIO.read(new File("")); // TODO: obtain path
-                case Wall wall -> ImageIO.read(new File("")); // TODO: obtain path
-        
-                default -> null; // TODO: obtain path to default
-            };
-        }catch(Exception e){return null;}
-    }
+  /**
+   * Converts Tile information to a usable Sprite
+   *
+   * @param tile 
+   */
+  public Sprite(Tile tile) {
+    this(resolveImage(tile), tile.location());
+  }
 
-    /**
-    * 
-    * @param tile an object that implements Tile
-    * @return the priority value corresponding to the type of tile
-    */
-    static int resolvePriority(Tile tile) {
-        return switch (tile) {
-            case Empty empty -> 1; // TODO: correct list of priorities
-            case Wall wall -> 1; // TODO: correct list of priorities
-        
-            default -> 0;
-        };
-    }
+  /**
+   * Converts Entity information to a usable Sprite
+   *
+   * @param entity 
+   */
+  public Sprite(Entity entity) {
+    this(resolveImage(entity), entity.location());
+  }
 
-    /**
-    * 
-    * 
-    * @return nothing
-    */
-    boolean draw() {
-        // renders image at its given position
-        return graphics.drawImage(image, (int)position.x(), (int)position.y(), 
-        32, 32, null);
+  /**
+   * Stores image and position. Position is offset by player position
+   * 
+   * @param image 
+   * @param position A Point representing 
+   */
+  private Sprite(BufferedImage image, Point position) {
+    this.image = image;
+    this.position = new Point(0, 0).sub(position); // TODO: offset by player pos
+  }
+
+  /**
+   * Used to determine 
+   * 
+   * @param o Either Tile or Entity, that corresponds to an image
+   * @return An image that Sprite uses to draw
+   */
+  private static BufferedImage resolveImage(Object o) {
+    // try-catch for file handling exceptions
+    try {
+      return switch (o) {
+        // Exit class image
+        case Exit exit ->
+            ImageIO.read(Sprite.class.getClassLoader().getResource("Finish Tile.png"));
+        // Wall class image
+        case Wall wall -> ImageIO.read(Sprite.class.getClassLoader().getResource("Wall.png"));
+        // Empty class image
+        case Empty empty -> ImageIO.read(Sprite.class.getClassLoader().getResource("tile.png"));
+        // Default image for when no individual case in place 
+        default -> ImageIO.read(Sprite.class.getClassLoader().getResource("placeholder.png")); // TODO: obtain path to default
+      };
+    } catch (IOException e) {
+      System.err.println("Sprite.resolveImage(Object), Failed to read file:\n"+e);
+      return null;
     }
+  }
+
+  /** 
+   * Renders the image at its given position with a fixed size
+  */
+  boolean draw() {
+    return graphics.drawImage(image, (int) position.x(), (int) position.y(), 32, 32, null);
+  }
 }
