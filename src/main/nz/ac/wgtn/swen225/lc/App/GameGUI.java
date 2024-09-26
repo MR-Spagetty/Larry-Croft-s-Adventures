a34c6fdb5ee204c4d301d36aa01a41fc8dc4143b
Developer 1 <dev1@example.internal>
2024-09-26 19:49:00 +1200

    Added comments into GameGUI.java

--- src/main/nz/ac/wgtn/swen225/lc/App/GameGUI.java
+++ src/main/nz/ac/wgtn/swen225/lc/App/GameGUI.java
@@ -6,7 +6,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 /**
- * TODO add comments
+ * Class which is responsible for handling the "Graphical User Interface" of the game.
  *
  * @author Developer 1 <dev1@example.internal>
  */
@@ -19,12 +19,16 @@ public class GameGUI extends JFrame{
     Runnable closeGame = ()->{};
 
     /*
-     * TODO add comments
+     * Action to be executed when the user begins playing a game. (i.e.: When we switch from the Start Menu to
+     * the Game Menu itself.) Currently, the Runnable action does not do anything, as what is executed is dependent
+     * on the components that are in the start menu (as these are removed).
      */
     Runnable changeGUIStyles = ()->{};
 
     /**
-     * TODO add comments
+     * Constructor of the Graphical User Interface, which is where the GUI is set up when you start up the game.
+     * This involves defining the size of the GUI window, and putting the Start Menu components inside.
+     * TODO: make a more professional version of the Start Menu GUI.
      */
     public GameGUI(){
         assert SwingUtilities.isEventDispatchThread();
@@ -48,7 +52,7 @@ public class GameGUI extends JFrame{
         JLabel instructions = StartScreen.instructions;
         JPanel buttons = StartScreen.createButtonsSection((unused -> changeGUIStyles.run()), (unused -> {}));
 
-        /**
+        /*
          * "changeGUIStyles" will be changed so when executed, the contents on the Start Menu are removed.
          * This is because this action will be run when a game is started.
          */
@@ -63,7 +67,8 @@ public class GameGUI extends JFrame{
     }
 
     /**
-     * TODO add comments
+     * Helper method which creates the components present in the Main menu.
+     * TODO: make a more professional version of the Main Menu GUI and add in the pane for displaying the graphics.
      */
     private void createMainMenu(){
         add(BorderLayout.NORTH, MainScreen.createGameButtons());
