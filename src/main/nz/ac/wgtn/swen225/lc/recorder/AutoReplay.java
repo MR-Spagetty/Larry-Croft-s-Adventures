6729e37ba3897c8001f1589daff6639fb4db2af4
Developer 4 <dev4@example.internal>
2024-09-26 23:43:25 +1200

    created AutoReplay class

---
+++ src/main/nz/ac/wgtn/swen225/lc/recorder/AutoReplay.java
@@ -0,0 +1,13 @@
+package nz.ac.wgtn.swen225.lc.recorder;
+
+import nz.ac.wgtn.swen225.lc.domain.PlayerAction;
+
+public class AutoReplay implements Replay{
+
+    @Override
+    public PlayerAction replay(int tick) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'replay'");
+    }
+    
+}
