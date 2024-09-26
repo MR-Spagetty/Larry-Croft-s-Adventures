222d1dd4f9b2e75133743452ebfab767a13d162c
Developer 1 <dev1@example.internal>
2024-09-26 12:43:32 +1200

    Removed garbage in KeyStrokes.java to allow for testing

--- src/main/nz/ac/wgtn/swen225/lc/App/KeyStrokes.java
+++ src/main/nz/ac/wgtn/swen225/lc/App/KeyStrokes.java
@@ -2,17 +2,7 @@ package nz.ac.wgtn.swen225.lc.app;
 
 import java.util.HashMap;
 import java.util.Map;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import java.util.Queue;
-import java.util.ArrayDeque;
->>>>>>> 10e265a33b0dba697421c1efb5e9365dfbaf9dca
-
-class Direction{} //JUST A MOCK CLASS!
-=======
 import nz.ac.wgtn.swen225.lc.domain.*;
->>>>>>> cec60e477dee09da3a17fc3144292e517c8ed323
 
 /**
  * Stores the keystrokes that associate with an action that is executed, and a direction that is set
@@ -21,21 +11,6 @@ import nz.ac.wgtn.swen225.lc.domain.*;
  * @author Developer 1 <dev1@example.internal>
  */
 public class KeyStrokes {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private final Map<Integer, Direction> strokeToDirection = new HashMap<>();
-    private final Map<Integer, Runnable> strokeToUIAction = new HashMap<>();
-
-=======
-    Direction active = null;
-    private final Map<Integer, Direction> strokeToDirection = new HashMap<>();
-
-    private final Map<Integer, Runnable> strokeToUIAction = new HashMap<>();
-
-    private final Queue<Integer> pendingKeyStrokes = new ArrayDeque<>();
-
->>>>>>> 10e265a33b0dba697421c1efb5e9365dfbaf9dca
-=======
     private final Map<Integer, PlayerAction> strokeToPlayerAction = new HashMap<>();
     private final Map<Integer, Runnable> strokeToUIAction = new HashMap<>();
 
@@ -43,7 +18,6 @@ public class KeyStrokes {
         return strokeToUIAction.containsKey(keyStroke);
     }
 
->>>>>>> cec60e477dee09da3a17fc3144292e517c8ed323
     /**
      * Binds a keystroke to a direction in which the character can move.
      *
@@ -71,25 +45,11 @@ public class KeyStrokes {
      * @param keyStroke The keystroke of the key that will be associated with the action.
      * @return The direction associated with the keystroke.
      */
-<<<<<<< HEAD
-    public Direction getDirection(int keyStroke){
-        Direction returnValue = strokeToDirection.get(keyStroke);
-<<<<<<< HEAD
-
-        if (returnValue == null)
-            throw new IllegalArgumentException("Given keystroke does not map to a direction!");
-
-=======
-        assert returnValue != null;
->>>>>>> 10e265a33b0dba697421c1efb5e9365dfbaf9dca
-        return returnValue;
-=======
     public PlayerAction getPlayerAction(int keyStroke){
         if (strokeToUIAction.get(keyStroke) != null)
             throw new IllegalCallerException("Given keystroke maps to a runnable Action!");
 
         return strokeToPlayerAction.getOrDefault(keyStroke, PlayerAction.None);
->>>>>>> cec60e477dee09da3a17fc3144292e517c8ed323
     }
 
     /**
@@ -99,38 +59,10 @@ public class KeyStrokes {
      * @param keyStroke The keystroke of the key that will be associated with the action.
      */
     public void performAction(int keyStroke){
-<<<<<<< HEAD
-<<<<<<< HEAD
-        if (strokeToDirection.get(keyStroke) != null) throw new IllegalCallerException("Given keystroke maps to a Direction!");
-
-        Runnable runAction = strokeToUIAction.get(keyStroke);
-
-        if (runAction == null)
-            throw new IllegalArgumentException("Given keystroke does not map to a runnable action!");
-=======
         if (strokeToPlayerAction.get(keyStroke) != null)
             throw new IllegalCallerException("Given keystroke maps to a Player Action!");
->>>>>>> cec60e477dee09da3a17fc3144292e517c8ed323
 
         Runnable runAction = strokeToUIAction.getOrDefault(keyStroke, () -> {});
         runAction.run();
     }
-<<<<<<< HEAD
-=======
-        Runnable runAction = strokeToUIAction.get(keyStroke);
-        runAction.run();
-    }
-
-    /**
-     * Every time a tick occurs, the action that is being performed or the direction in which the
-     * character is moving stops moving, and the next action/direction is selected.
-     */
-    public void tick(){
-        active = null;
-        if (!pendingKeyStrokes.isEmpty()) active = getDirection(pendingKeyStrokes.poll());
-        pendingKeyStrokes.clear();
-    }
->>>>>>> 10e265a33b0dba697421c1efb5e9365dfbaf9dca
-=======
->>>>>>> cec60e477dee09da3a17fc3144292e517c8ed323
 }
