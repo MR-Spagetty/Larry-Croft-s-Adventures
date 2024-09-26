64d86ea96bc21dffd7be8a212e264377513db5e0
Developer 5 <dev5@example.internal>
2024-09-26 21:52:33 +1200

    Changed name in JSONSerializable file

--- src/main/nz/ac/wgtn/swen225/lc/persistency/JSONSerializable.java
+++ src/main/nz/ac/wgtn/swen225/lc/persistency/JSONSerializable.java
@@ -1,6 +1,6 @@
 package nz.ac.wgtn.swen225.lc.persistency;
 
-public interface JsonSerializable <T>{
-  String toJson();  // Serialize object to JSON
-  T fromJson(String json);  // Deserialize object from JSON
+public interface JSONSerializable <T>{
+  JSONType toJson();  // Serialize object to JSON
+  T fromJson(JSONType json);  // Deserialize object from JSON
 }
