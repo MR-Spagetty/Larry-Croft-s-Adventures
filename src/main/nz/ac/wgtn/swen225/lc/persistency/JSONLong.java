9738f790dee325e7bae413c9cdd9da4d7f26c3b0
Developer 5 <dev5@example.internal>
2024-09-26 14:58:18 +1200

    Splitting JSOnObject class
    JSONObject class is now being split into multiple classes for each object type, starting with JSONLong class for Longs

--- src/main/nz/ac/wgtn/swen225/lc/persistency/JSONLong.java
+++ src/main/nz/ac/wgtn/swen225/lc/persistency/JSONLong.java
@@ -9,25 +9,10 @@ import java.util.Map;
 // This allows selective serialization of key-value pairs.
 // For e.g. only certain fields of a Player object might need to be saved to JSON.
 
-public class JSONObject implements JSONType {
-  private static Map<String, Object> dataForObject = new HashMap<>();
+public class JSONLong implements JSONType {
+  private static Map<String, Long> dataForObject = new HashMap<>();
   // Strings, booleans, int, longs etc.
-  public void add(String key, String value) {
-    dataForObject.put(key, value);
-  }
-
   public void add(String key, Long value) {
-    dataForObject.put(key, value);
-  }
-
-  public void add(String key, int value) {
-    dataForObject.put(key, value);
-  }
-  public void add(String key, double value) {
-    dataForObject.put(key, value);
-  }
-
-  public void add(String key, JSONType value) {
     dataForObject.put(key, value);
   }
 
