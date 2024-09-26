d29f6d5e0d58d1db5f47b9d24a42fbb6e1ba4b71
Developer 1 <dev1@example.internal>
2024-09-26 12:38:40 +1200

    Begin rearranging code in GameGUI.java due to following "single GUI" approach

--- src/main/nz/ac/wgtn/swen225/lc/App/GameGUI.java
+++ src/main/nz/ac/wgtn/swen225/lc/App/GameGUI.java
@@ -1,6 +1,7 @@
 package nz.ac.wgtn.swen225.lc.app;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -10,27 +11,38 @@ import java.awt.event.WindowEvent;
  */
 public class GameGUI extends JFrame{
     /*
-    * Action to be executed when the user closes the Game GUI with the 'X' button.
-    * This action will be mostly similar to quitting the current game playing, as you will also be
-    * asked whether you want to save the game before quitting.
-    */
+     * Action to be executed when the user closes the Game GUI with the 'X' button.
+     * This action will be mostly similar to quitting the current game playing, as you will also be
+     * asked whether you want to save the game before quitting.
+     */
     Runnable closeGame= ()->{};
 
+    public GameGUI(){
+        assert SwingUtilities.isEventDispatchThread();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        addWindowListener(new WindowAdapter(){
+            public void windowClosed(WindowEvent e){ closeGame.run(); }
+        });
+
+        setPreferredSize(new Dimension(1200, 600));
+        pack();
+    }
+}
+
+class GameScreen extends JFrame{
     //The Screen that will be displayed when the game is paused.
     static PauseScreen ps = new PauseScreen(200);
 
     /**
      * Constructor for the Game GUI.
      */
-    public GameGUI(){
+    public GameScreen(){
         assert SwingUtilities.isEventDispatchThread();
 
         setVisible(true);
-        addWindowListener(new WindowAdapter(){
-            public void windowClosed(WindowEvent e){ closeGame.run(); }
-        });
 
-        ControlKeys keys = new ControlKeys(); //Initialises the class specifically for controlling the
+        new ControlKeys(); //Initialises the class specifically for controlling the keys.
     }
 
     public void createGameInfo(){
