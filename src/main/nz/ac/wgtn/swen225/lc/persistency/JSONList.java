b93816158720474e2e714a7c18604635c312d378
Developer 5 <dev5@example.internal>
2024-09-26 22:53:32 +1200

    Updated JSONList to include different types

--- src/main/nz/ac/wgtn/swen225/lc/persistency/JSONList.java
+++ src/main/nz/ac/wgtn/swen225/lc/persistency/JSONList.java
@@ -1,5 +1,24 @@
 package nz.ac.wgtn.swen225.lc.persistency;
 
-public record JSONList(String value) implements JSONType {
-  //
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONList implements JSONType {
+
+  // List to hold multiple JSONType elements
+  private List<JSONType> elements = new ArrayList<>();
+
+
+  //public void add(JSONType element) { elements.add(element); }
+
+  // Overload add methods for each type
+  public void add(String value) { elements.add(new JSONString(value)); }
+  public void add(Long value) { elements.add(new JSONLong(value)); }
+  public void add(Double value) { elements.add(new JSONDouble(value)); }
+  public void add(Boolean value) { elements.add(new JSONBool(value)); }
+  public void add(JSONObject value) { elements.add(value); }
+  public void add(JSONList value) { elements.add(value); }
+
+  // Return the list of elements
+  public List<JSONType> getElements() { return elements; }
 }
