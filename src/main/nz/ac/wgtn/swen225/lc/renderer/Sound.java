6548e6e09453661a1a1e41e4c98def500d9ff316
Developer 2 <dev2@example.internal>
2024-09-30 15:11:00 +1300

    Sound handler skeleton

--- src/main/nz/ac/wgtn/swen225/lc/renderer/Sound.java
+++ src/main/nz/ac/wgtn/swen225/lc/renderer/Sound.java
@@ -1,5 +1,32 @@
 package nz.ac.wgtn.swen225.lc.renderer;
 
+import java.io.IOException;
+import javax.sound.sampled.*;
+
 public class Sound {
-    
+    private AudioInputStream sound;
+
+    public Sound(Object o) {
+        this.sound = resolveSound(o);
+    }
+
+    private AudioInputStream resolveSound(Object o) {
+        try{
+            return switch (o) {
+                case Sound s -> AudioSystem.getAudioInputStream(Sound.class.getClassLoader().getResource("placeholder.wav"));
+                default -> null;
+            };
+        }catch(UnsupportedAudioFileException | IOException e){
+            System.err.println("Sound.resolveSound(Object), failed to read file:\n" + e);
+            return null;
+        }
+    }
+    public void play() {
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(sound);
+        }catch(LineUnavailableException | IOException e){
+            System.err.println("Sound.play(), failed to read file:\n" + e);
+        }
+    }
 }
