b1622fbb9e070761c7da48d721fc1b0b51859ced
Developer 1 <dev1@example.internal>
2024-09-30 13:55:25 +1300

    Removed faulty directory after cloning

---
+++ src/tests/test/nz/ac/wgtn/swen225/lc/app2/KeystrokeTests.java
@@ -0,0 +1,54 @@
+package test.nz.ac.wgtn.swen225.lc.app;
+
+import main.nz.ac.wgtn.swen225.lc.app.ControlKeys;
+import main.nz.ac.wgtn.swen225.lc.domain.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.awt.event.KeyEvent;
+
+public class KeystrokeTests{
+    ControlKeys keys = new ControlKeys();
+    Class<IllegalCallerException> ice = IllegalCallerException.class;
+
+    @Test void assertionsEnabled(){
+        Assertions.assertThrows(AssertionError.class, () -> { assert false; });
+    }
+
+    @Test void strokeDoesntMapAnywhere(){
+        Assertions.assertEquals(getActivePlayerAction(), PlayerAction.None);
+        keys.setNextKeyStroke(KeyEvent.VK_G); //"G" is not assigned to a Player action in the game.
+        setPlayerActionAtTick();
+        Assertions.assertEquals(getActivePlayerAction(), PlayerAction.None);
+    }
+
+    @Test void strokeMapsToUIAction(){
+        keys.setNextKeyStroke(KeyEvent.VK_1); //"1" is mapped to a Runnable action, not a Player action.
+        Assertions.assertThrows(ice, setPlayerActionAtTick());
+    }
+
+    @Test void strokeCorrectlyMaps1(){
+        keys.setNextKeyStroke(KeyEvent.VK_KP_UP); //"UP" is mapped to the Player action for going up.
+        setPlayerActionAtTick();
+        Assertions.assertEquals(getActivePlayerAction(), PlayerAction.Up);
+    }
+
+    @Test void strokeCorrectlyMaps2(){
+        keys.setNextKeyStroke(KeyEvent.VK_KP_LEFT);
+        setPlayerActionAtTick();
+        Assertions.assertEquals(getActivePlayerAction(), PlayerAction.Left);
+    }
+
+    @Test void strokeCorrectlyMaps3(){
+        keys.setNextKeyStroke(KeyEvent.VK_KP_RIGHT);
+        setPlayerActionAtTick();
+        Assertions.assertEquals(getActivePlayerAction(), PlayerAction.Right);
+    }
+
+    @Test void strokeCorrectlyMaps4(){
+        keys.setNextKeyStroke(KeyEvent.VK_KP_DOWN);
+        setPlayerActionAtTick();
+        Assertions.assertEquals(getActivePlayerAction(), PlayerAction.Down);
+    }
+}
