694b3d23bc9810ebcbbc0e591c8bae47a028c14a
Developer 3 <dev3@example.internal>
2024-10-06 21:49:45 +1300

    created fuzz package and class, nothing else can be done for now

---
+++ src/main/nz/ac/wgtn/swen225/lc/fuzz/inputTest.java
@@ -0,0 +1,12 @@
+package nz.ac.wgtn.swen225.lc.fuzz;
+
+import nz.ac.wgtn.swen225.lc.app.*;
+
+import java.util.Random;
+
+public class inputTest {
+  ControlKeys getControl(){
+    throw new UnsupportedOperationException("Not supported yet.");
+    //TODO get the control keys and start using them lol??? daniel hasnt done anything for use yet so idk lol
+  }
+}
