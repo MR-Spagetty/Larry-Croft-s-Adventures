f47ff7186e8fda562ab7f4d4240808da915d73c7
Developer 3 <dev3@example.internal>
2024-10-07 12:39:40 +1300

    fixed naming convention of Input Test

--- src/main/nz/ac/wgtn/swen225/lc/fuzz/InputTest.java
+++ src/main/nz/ac/wgtn/swen225/lc/fuzz/InputTest.java
@@ -3,14 +3,12 @@ package nz.ac.wgtn.swen225.lc.fuzz;
 import nz.ac.wgtn.swen225.lc.app.*;
 
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /* inputTest takes in all the defined user inputs and randomly triggers them to test if anything is kill */
-public class inputTest {
-  List<KeyEvent> userKeys = List.of(
-
-  );
+public class InputTest {
+  List<KeyEvent> userKeys = new ArrayList<>();  //variable that will store all the user inputs
   ControlKeys getControl(){
     throw new UnsupportedOperationException("Not supported yet.");
     //TODO get the control keys and start using them lol??? daniel hasnt done anything for use yet so idk lol
