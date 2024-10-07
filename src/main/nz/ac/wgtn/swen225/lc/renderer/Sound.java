84d3d996f5020bd248249bb4e5cb8787686b7582
Developer 2 <dev2@example.internal>
2024-10-07 14:22:23 +1300

    Sound handler setup for sounds to be added

--- src/main/nz/ac/wgtn/swen225/lc/renderer/Sound.java
+++ src/main/nz/ac/wgtn/swen225/lc/renderer/Sound.java
@@ -1,32 +1,51 @@
 package nz.ac.wgtn.swen225.lc.renderer;
 
 import java.io.IOException;
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Sound {
-    private AudioInputStream sound;
+  private AudioInputStream sound;
 
-    public Sound(Object o) {
-        this.sound = resolveSound(o);
-    }
+  /**
+   * Description TODO
+   *
+   * @param o
+   */
+  public Sound(Object o) {
+    this.sound = resolveSound(o);
+  }
 
-    private AudioInputStream resolveSound(Object o) {
-        try{
-            return switch (o) {
-                case Sound s -> AudioSystem.getAudioInputStream(Sound.class.getClassLoader().getResource("placeholder.wav"));
-                default -> null;
-            };
-        }catch(UnsupportedAudioFileException | IOException e){
-            System.err.println("Sound.resolveSound(Object), failed to read file:\n" + e);
-            return null;
-        }
+  /**
+   * Description TODO
+   *
+   * @param o
+   * @return
+   */
+  private AudioInputStream resolveSound(Object o) {
+    try {
+      return switch (o) {
+        case Sound s ->
+            AudioSystem.getAudioInputStream(
+                Sound.class.getClassLoader().getResource("placeholder.wav"));
+        default -> null;
+      };
+    } catch (UnsupportedAudioFileException | IOException e) {
+      System.err.println("Sound.resolveSound(Object):\n" + e);
+      return null;
     }
-    public void play() {
-        try{
-            Clip clip = AudioSystem.getClip();
-            clip.open(sound);
-        }catch(LineUnavailableException | IOException e){
-            System.err.println("Sound.play(), failed to read file:\n" + e);
-        }
+  }
+
+  /** Description TODO */
+  public void play() {
+    try {
+      Clip clip = AudioSystem.getClip();
+      clip.open(sound);
+    } catch (LineUnavailableException | IOException e) {
+      System.err.println("Sound.play():\n" + e);
     }
+  }
 }
