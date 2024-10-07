101ad60b3f01b2c4d5fa574dd9841421f8bcfb9e
Developer 3 <dev3@example.internal>
2024-10-07 13:47:21 +1300

    added a comment

--- src/main/nz/ac/wgtn/swen225/lc/fuzz/InputTest.java
+++ src/main/nz/ac/wgtn/swen225/lc/fuzz/InputTest.java
@@ -6,12 +6,21 @@ import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-/* inputTest takes in all the defined user inputs and randomly triggers them to test if anything is kill */
+/* inputTest takes in all the defined user inputs
+ and randomly triggers them to test if anything is kill */
 public class InputTest {
   List<KeyEvent> userKeys = new ArrayList<>();  //variable that will store all the user inputs
-  ControlKeys getControl(){
+  ControlKeys getControl(){ //getter method for grabbing the defined userKeys in game
     throw new UnsupportedOperationException("Not supported yet.");
     //TODO get the control keys and start using them lol??? daniel hasnt done anything for use yet so idk lol
     //TODO grab the control keys
   }
+
+  void keyTester(){     //cycle through the entire list of userKeys with a try catch
+    if(userKeys.isEmpty()){
+      throw new IllegalArgumentException("UserKeys are empty or undefined");
+    }
+
+  }
+
 }
