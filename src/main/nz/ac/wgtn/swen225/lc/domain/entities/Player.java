fd107273e81c4a92c343955e8f9b3ff790eb7bbf
Developer 1 <dev1@example.internal>
2024-10-08 20:54:07 +1300

    Removed accidentally commited change to Player.java

--- src/main/nz/ac/wgtn/swen225/lc/domain/entities/Player.java
+++ src/main/nz/ac/wgtn/swen225/lc/domain/entities/Player.java
@@ -182,7 +182,7 @@ public class Player implements MoveableEntity {
     return this.dead;
   }
 
-  public PlayerAction getFacing() {
+  PlayerAction getFacing() {
     Point move = lastMove().limit(1l);
     if (move.equals(Point.ORIGIN)) {
       return PlayerAction.None;
