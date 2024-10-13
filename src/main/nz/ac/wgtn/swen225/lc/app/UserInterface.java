456288286cf4167bccc8a022799adfb7220816af
Developer 1 <dev1@example.internal>
2024-10-14 10:57:08 +1300

    Finished refactoring the Start UI

--- src/main/nz/ac/wgtn/swen225/lc/app/UserInterface.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/UserInterface.java
@@ -1,9 +1,11 @@
 package nz.ac.wgtn.swen225.lc.app;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
 
 /**
  * Class which is responsible for handling the "Graphical User Interface" of the game.
@@ -19,17 +21,13 @@ public class UserInterface extends JFrame{
     Runnable closeGame = ()->{};
 
     /*
-     * Action to be executed when the user begins playing a game. (i.e.: When we switch from the Start Menu to
-     * the Game Menu itself.) Currently, the Runnable action does not do anything, as what is executed is dependent
-     * on the components that are in the start menu (as these are removed).
-     */
-    Runnable startGame = ()->{};
-
-    /*
      * Timer mainly for determining when to trigger the "draw" mechanism in the Renderer. This timer is static, so
      * the Pause Screen can stop and start it to "technically" pause the game.
      */
     static Timer timer;
+
+    //Executed when the player wants to start a game. It basically removes all of the Start UI components from the frame.
+    Runnable removeStartUI = () -> {};
 
     GameUI gameControls; //The wider "Game UI" that the user will be interacting with!
     ControlKeys keyControl; //An instance of the "Key Controller" they will be interacting with.
@@ -40,7 +38,6 @@ public class UserInterface extends JFrame{
     /**
      * Constructor of the Graphical User Interface, which is where the GUI is set up when you start up the game.
      * This involves defining the size of the GUI window, and putting the Start Menu components inside.
-     * TODO: make a more professional version of the Start Menu GUI.
      */
     public UserInterface(){
         assert SwingUtilities.isEventDispatchThread();
@@ -61,17 +58,10 @@ public class UserInterface extends JFrame{
     }
 
     /**
-     * Helper method that creates the "components" that will be in the Start Menu.
-     */
-    private void createStartMenu(){
-        new StartUI();
-    }
-
-    /**
      * Helper method which creates the components present in the Main menu. This also sets up the keys to be used
      * in the game.
      */
-    protected void createMainMenu(){
+    private void createMainMenu(){
         this.add(BorderLayout.EAST, gameControls.createMenu());
         this.addKeyListener(keyControl);
 
@@ -86,5 +76,84 @@ public class UserInterface extends JFrame{
 
         timer.start();
          */
+    }
+
+    /**
+     * Helper method which creates the components present in the Start Menu, including the Buttons and their
+     * corresponding actions.
+     */
+    private void createStartMenu(){
+        JPanel instructions = Instructions.instructionsPanel;
+        JPanel buttons = createButtonsSection();
+
+        //When we start or resume a game, we will need to remove all the Start UI elements, so we can add the Game UI in.
+        removeStartUI = () -> {
+            remove(instructions); remove(buttons);
+            SwingUtilities.updateComponentTreeUI(this); //Refreshes the JFrame after the objects are removed!
+        };
+
+        add(BorderLayout.NORTH, instructions);
+        add(BorderLayout.CENTER, buttons);
+    }
+
+    /**
+     * Creates a "JPanel" that will hold the buttons of the Start Menu. One of the buttons will start a new
+     * game for the player, and the other will allow the player to select an existing game to resume.
+     */
+    private JPanel createButtonsSection(){
+        JPanel buttons = new JPanel();
+        buttons.add(new DefaultButton((unused -> startNewGame()), "Start new game!"));
+        buttons.add(new DefaultButton(unused -> resumeExistingGame(), "Resume existing game!"));
+
+        return buttons;
+    }
+
+    /**
+     * Loads and automatically resumes an existing game from a ".json" file. This process is cancelled if the user
+     * terminates the loading of a file.
+     */
+    private void resumeExistingGame(){
+        File fileToLoad = loadExistingGame();
+
+        if (fileToLoad != null){
+            removeStartUI.run();
+            createMainMenu();
+        }
+    }
+
+    /** Creates a new game and runs it. */
+    private void startNewGame(){
+        removeStartUI.run();
+        createMainMenu();
+    }
+
+    /**
+     * Loads an existing game from a ".json" file using the JFileChooser mechanism. You are repeatedly asked for a
+     * file until you either select a valid file, or if you decide to abandon selecting a valid file.
+     * ===
+     * References:
+     * https://www.tutorialspoint.com/get-the-path-of-the-file-selected-in-the-jfilechooser-component-with-java
+     * https://www.geeksforgeeks.org/java-swing-jfilechooser/
+     */
+    private File loadExistingGame(){
+        boolean validFileSelected = false;
+        File chosenFile = null;
+
+        while (!validFileSelected){
+            JFileChooser chooseFile = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+            int result = chooseFile.showOpenDialog(null);
+
+            if (result != JFileChooser.APPROVE_OPTION) return null; //If you decide to cancel the operation.
+
+            chosenFile = chooseFile.getSelectedFile();
+
+            if (chosenFile.getName().contains(".json")){ validFileSelected = true; }
+            else {
+                JOptionPane.showMessageDialog(null, "Invalid File Selected! Only \".json\" files can be selected!",
+                        "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+
+        return chosenFile;
     }
 }
