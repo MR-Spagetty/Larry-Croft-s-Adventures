77c03e8a677e9f3461cff563e61c9bca2d4b7a15
Developer 1 <dev1@example.internal>
2024-10-15 22:23:09 +1300

    Moved "loadExistingGame()" into new class

--- src/main/nz/ac/wgtn/swen225/lc/app/UserInterface.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/UserInterface.java
@@ -167,6 +167,27 @@ public class UserInterface extends JFrame{
         removeGameUI.run();
         createStartMenu();
     }
+}
+
+class InputController {
+    List<DefaultButton> mainUIButtons;
+    ControlKeys keyController;
+
+    //To prevent more than one "Lite" User Interface instance from being created.
+    private static final InputController IC = new InputController();
+    public static InputController ic = IC;
+
+    private InputController(){
+        mainUIButtons = Buttons.mainUIButtons(() -> endGame(true), () -> endGame(false), () -> {});
+
+        keyController= new ControlKeys(Map.of(
+                "EXIT", () -> endGame(false),
+                "SAVE", () -> endGame(true),
+                "RESUME", this::resumeExistingGameFromCurrentGame,
+                "PAUSE", Buttons::pauseGame,
+                "S_REPLAY", App::callStepReplay
+        ));
+    }
 
     /**
      * Loads an existing game from a ".json" file using the JFileChooser mechanism. You are repeatedly asked for a
@@ -196,26 +217,5 @@ public class UserInterface extends JFrame{
         }
 
         return chosenFile;
-    }
-}
-
-class InputController {
-    List<DefaultButton> mainUIButtons;
-    ControlKeys keyController;
-
-    //To prevent more than one "Lite" User Interface instance from being created.
-    private static final InputController IC = new InputController();
-    public static InputController ic = IC;
-
-    private InputController(){
-        mainUIButtons = Buttons.mainUIButtons(() -> endGame(true), () -> endGame(false), () -> {});
-
-        keyController= new ControlKeys(Map.of(
-                "EXIT", () -> endGame(false),
-                "SAVE", () -> endGame(true),
-                "RESUME", this::resumeExistingGameFromCurrentGame,
-                "PAUSE", Buttons::pauseGame,
-                "S_REPLAY", App::callStepReplay
-        ));
     }
 }
