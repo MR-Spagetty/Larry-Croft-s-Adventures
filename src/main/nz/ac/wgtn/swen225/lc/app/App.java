388f0b3505bac730c4a6823b36de3317d225b079
Developer 1 <dev1@example.internal>
2024-10-16 00:00:32 +1300

    Rolled forward to changes after refactoring

--- src/main/nz/ac/wgtn/swen225/lc/app/App.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/App.java
@@ -7,7 +7,6 @@ import nz.ac.wgtn.swen225.lc.recorder.*;
 import javax.swing.*;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -85,19 +84,19 @@ public class App{
      * This method returns the list of buttons that have been created in the game. This method is specifically
      * for the purpose of allowing the "Fuzz" module to access the buttons.
      */
-    public static List<DefaultButton> getButtons(){ return Collections.unmodifiableList(IOController.ic.mainUIButtons); }
+    public static List<DefaultButton> getButtons(){ return IOController.ic.getMainUIButtons(); }
 
     /** Returns the list of keystrokes associated with an action. */
-    public static Set<Integer> getKeyStrokes(){ return IOController.ic.keyController.getKeyStrokes(); }
+    public static Set<Integer> getKeyStrokes(){ return IOController.ic.getKeyController().getKeyStrokes(); }
 
     /** Returns an unmodifiable map of the keystrokes mapped to their player actions. */
     public static Map<Integer, PlayerAction> strokesToPlayerAction(){
-        return IOController.ic.keyController.strokesToPlayerAction();
+        return IOController.ic.getKeyController().strokesToPlayerAction();
     }
 
     /** Returns an unmodifiable map of the keystrokes mapped to UI actions. */
     public static Map<Integer, Runnable> strokesToUIAction(){
-        return IOController.ic.keyController.strokesToUIAction();
+        return IOController.ic.getKeyController().strokesToUIAction();
     }
 
     /**
--- src/main/nz/ac/wgtn/swen225/lc/app/UserInterface.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/UserInterface.java
@@ -1,8 +1,11 @@
 package nz.ac.wgtn.swen225.lc.app;
 
+import nz.ac.wgtn.swen225.lc.domain.GameState;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.util.Collections;
 import java.util.List;
 import java.io.File;
 import java.util.Map;
@@ -37,7 +40,7 @@ public class UserInterface extends JFrame{
      * setup. The setup needs to be done in a method called "createMenu".
      */
     private UserInterface(){
-        //createMenu();
+        createMenu();
     }
 
     /**
@@ -61,7 +64,7 @@ public class UserInterface extends JFrame{
      */
     private void createStartMenu(){
         JPanel instructions = Instructions.instructionsPanel;
-        JPanel buttons = Buttons.startUIButtonPanel(() -> startGame(null), () -> IOController.ic.resumeExistingGame());
+        JPanel buttons = startUIButtonPanel(() -> startGame(null), () -> IOController.ic.resumeExistingGame());
 
         removeStartUI = () -> {
             remove(instructions); remove(buttons);
@@ -77,7 +80,9 @@ public class UserInterface extends JFrame{
      * in the game.
      */
     private void createMainMenu(){
-        GameUI gameControls = new GameUI(Color.DARK_GRAY, WIDTH/4, HEIGHT, IOController.ic.mainUIButtons); //The wider "Game UI" that the user will be interacting with!
+        //The wider "Game UI" that the user will be interacting with!
+        GameUI gameControls = new GameUI(Color.DARK_GRAY, WIDTH/4, HEIGHT, IOController.ic.getMainUIButtons());
+
         GraphicsPane pane = new GraphicsPane();
 
         removeGameUI = () -> {
@@ -86,7 +91,7 @@ public class UserInterface extends JFrame{
         };
 
         this.add(BorderLayout.EAST, gameControls);
-        this.addKeyListener(IOController.ic.keyController);
+        this.addKeyListener(IOController.ic.getKeyController());
 
         this.setFocusable(true);
         this.requestFocus();
@@ -102,8 +107,8 @@ public class UserInterface extends JFrame{
         if (gameFile == null) gameFile = new File("src/main/nz/ac/wgtn/swen225/lc/persistency/examplelvl1.json");
 
         removeStartUI.run();
-        //GameState.getGameState().setLevel(gameFile.toPath());
-        //createMainMenu();
+        GameState.getGameState().setLevel(gameFile.toPath());
+        createMainMenu();
     }
 
     protected void saveGame(){
@@ -121,100 +126,17 @@ public class UserInterface extends JFrame{
         removeGameUI.run();
         createStartMenu();
     }
-}
-
-class IOController {
-    List<DefaultButton> mainUIButtons;
-    ControlKeys keyController;
-
-    //To prevent more than one "Lite" User Interface instance from being created.
-    private static final IOController IC = new IOController();
-    public static IOController ic = IC;
-
-    private IOController() {
-        mainUIButtons = Buttons.mainUIButtons(() -> endGame(true), () -> endGame(false), () -> {});
-
-        keyController = new ControlKeys(Map.of(
-                "EXIT", () -> endGame(false),
-                "SAVE", () -> endGame(true),
-                "RESUME", this::resumeExistingGameFromCurrentGame,
-                "PAUSE", Buttons::pauseGame,
-                "S_REPLAY", App::callStepReplay
-        ));
-    }
 
     /**
-     * Finishes up an already-started game and returns the user back to the main menu. If selected, the current game will also
-     * be saved.
-     *
-     * @param save Whether the current game will be saved to a file or not!
+     * Creates the "JPanel" that will hold the buttons of the Start Menu. One of the buttons will start a new
+     * game for the player, and the other will allow the player to select an existing game to resume.
      */
-    public void endGame(boolean save){
-        if (save){
-            UserInterface.ui.saveGame();
-        } else {
-            int result = JOptionPane.showConfirmDialog(
-                    null, "Are you sure want to exit without saving?",
-                    "Confirm", JOptionPane.YES_NO_OPTION
-            );
+    private JPanel startUIButtonPanel(Runnable startGame, Runnable resumeGame){
+        JPanel buttons = new JPanel();
 
-            if (result == JOptionPane.NO_OPTION) return;
-        }
+        buttons.add(new DefaultButton(unused -> startGame.run(), "Start new game!"));
+        buttons.add(new DefaultButton(unused -> resumeGame.run(), "Resume existing game!"));
 
-        UserInterface.ui.endGame();
-    }
-
-    /**
-     * Similar to the above method "resumeExistingGame", except it checks to make sure you're OK with exiting
-     * the current game before asking you to select a game file.
-     */
-    protected void resumeExistingGameFromCurrentGame(){
-        int result = JOptionPane.showConfirmDialog(
-                null, "Are you sure want to exit without saving?",
-                "Confirm", JOptionPane.YES_NO_OPTION
-        );
-
-        if (result == JOptionPane.NO_OPTION) return;
-
-        resumeExistingGame();
-    }
-
-    /**
-     * Loads and automatically resumes an existing game from a ".json" file. This process is cancelled if the user
-     * terminates the loading of a file.
-     */
-    protected void resumeExistingGame(){
-        File fileToLoad = loadExistingGame();
-        if (fileToLoad != null) UserInterface.ui.startGame(fileToLoad);
-    }
-
-    /**
-     * Loads an existing game from a ".json" file using the JFileChooser mechanism. You are repeatedly asked for a
-     * file until you either select a valid file, or if you decide to abandon selecting a valid file.
-     * ===
-     * References:
-     * https://www.tutorialspoint.com/get-the-path-of-the-file-selected-in-the-jfilechooser-component-with-java
-     * https://www.geeksforgeeks.org/java-swing-jfilechooser/
-     */
-    private File loadExistingGame(){
-        boolean validFileSelected = false;
-        File chosenFile = null;
-
-        while (!validFileSelected){
-            JFileChooser chooseFile = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            int result = chooseFile.showOpenDialog(null);
-
-            if (result != JFileChooser.APPROVE_OPTION) return null; //If you decide to cancel the operation.
-
-            chosenFile = chooseFile.getSelectedFile();
-
-            if (chosenFile.getName().contains(".json")){ validFileSelected = true; }
-            else {
-                JOptionPane.showMessageDialog(null, "Invalid File Selected! Only \".json\" files can be selected!",
-                        "ERROR", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-
-        return chosenFile;
+        return buttons;
     }
 }
