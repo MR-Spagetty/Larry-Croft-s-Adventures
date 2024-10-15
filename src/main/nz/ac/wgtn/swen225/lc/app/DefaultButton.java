1c8f338983880136b7972ec3eb2592a3b879b4f6
Developer 1 <dev1@example.internal>
2024-10-15 18:59:09 +1300

    Added JavaDoc comments

--- src/main/nz/ac/wgtn/swen225/lc/app/DefaultButton.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/DefaultButton.java
@@ -8,11 +8,23 @@ import java.awt.event.ActionListener;
  * automatically assigned with an Action Listener and have it be enabled by default.
  */
 public class DefaultButton extends JToggleButton{
+    /**
+     * Constructor used when the Button we are creating will be a button with "text" in it.
+     *
+     * @param al The action that will be executed when the button is pressed.
+     * @param text The text label of the button.
+     */
     public DefaultButton(ActionListener al, String text){
         super(text); //Calls the "JToggleButton" constructor and adds in the set text.
         restOfConstructor(al);
     }
 
+    /**
+     * Constructor used when the Button we are creating will be a button with "text" in it.
+     *
+     * @param al The action that will be executed when the button is pressed.
+     * @param img The icon (in the form of an image) label of the button.
+     */
     public DefaultButton(ActionListener al, ImageIcon img){
         super(img);
         restOfConstructor(al);
@@ -22,6 +34,6 @@ public class DefaultButton extends JToggleButton{
     private void restOfConstructor(ActionListener al){
         this.addActionListener(al);
         this.setEnabled(true); //The button will be enabled by default.
-        this.setFocusable(false);
+        this.setFocusable(false); //To prevent the focus of the keyboard turning to the button.
     }
 }
