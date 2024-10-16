c1b262df6b973d2929ce7d8195d53545ccd708e5
Developer 1 <dev1@example.internal>
2024-10-16 23:01:38 +1300

    Added JavaDocs into GridPanel.java

--- src/main/nz/ac/wgtn/swen225/lc/app/panels/GridPanel.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/panels/GridPanel.java
@@ -8,11 +8,30 @@ import java.awt.*;
  * than in a normal fashion.
  */
 public class GridPanel extends JPanel{
+
+    /**
+     * Constructor used when you want the resulting Grid Panel to be of a preferred size in width and height.
+     *
+     * @param backgroundColor The background colour of the Grid JPanel. Can be "null" if no background is to
+     *                        be set.
+     * @param width The preferred width of the Grid JPanel.
+     * @param height The preferred height of the Grid JPanel.
+     * @param rows The number of rows in the "Grid" part of the JPanel.
+     * @param cols The number of columns in the "Grid" part of the JPanel.
+     */
     public GridPanel(Color backgroundColor, int width, int height, int rows, int cols){
         this(backgroundColor, rows, cols);
         this.setPreferredSize(new Dimension(width, height));
     }
 
+    /**
+     * Constructor used when the size of the resulting Grid Panel doesn't matter.
+     *
+     * @param backgroundColor The background colour of the Grid JPanel. Can be "null" if no background is to
+     *                        be set.
+     * @param rows The number of rows in the "Grid" part of the JPanel.
+     * @param cols The number of columns in the "Grid" part of the JPanel.
+     */
     public GridPanel(Color backgroundColor, int rows, int cols){
         this.setBackground(backgroundColor);
         this.setLayout(new GridLayout(rows, cols, 4, 4));
