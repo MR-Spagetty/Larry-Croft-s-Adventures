d3a4831b1c6131862d17197043825d249b961956
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2024-10-16 13:42:25 +1300

    Made Empty tile JSON deserializable

--- src/main/nz/ac/wgtn/swen225/lc/domain/tiles/Empty.java
+++ src/main/nz/ac/wgtn/swen225/lc/domain/tiles/Empty.java
@@ -2,6 +2,8 @@ package nz.ac.wgtn.swen225.lc.domain.tiles;
 
 
 import nz.ac.wgtn.swen225.lc.domain.Point;
+import nz.ac.wgtn.swen225.lc.persistency.JSONObject;
+import nz.ac.wgtn.swen225.lc.persistency.JSONString;
 
 public class Empty extends AbstractTile {
 
@@ -12,5 +14,13 @@ public class Empty extends AbstractTile {
    */
   public Empty(Point location) {
     super(location);
+  }
+
+  public static Empty fromJSON(JSONObject json) {
+    if (!((JSONString) json.get("tile")).get().equals("Conveyor")) {
+      throw new IllegalArgumentException(
+          "Incorrect data given expected Conveyor got: " + json.get("Tile"));
+    }
+    return new Empty(Point.fromJSON(json.get("position")));
   }
 }
