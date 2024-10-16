31cd26d64c4c7e7682ca9378680899fb09a452f0
Developer 1 <dev1@example.internal>
2024-10-16 23:06:15 +1300

    Added JavaDocs into GameInventoryPanel.java

--- src/main/nz/ac/wgtn/swen225/lc/app/panels/GameInventoryPanel.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/panels/GameInventoryPanel.java
@@ -11,6 +11,9 @@ public class GameInventoryPanel extends GridPanel {
     /**
      * Constructor of the Inventory Panel, where the content of the Panel is loaded in.
      * TODO: Investigate how the "Inventory icons" will be loaded.
+     *
+     * @param width The width of the Inventory Panel.
+     * @param height The preferred height of the Inventory Panel.
      */
     public GameInventoryPanel(int width, int height){
         super(Color.LIGHT_GRAY, width, height, 2, 3);
