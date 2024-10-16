60746b10961a6f9e438949c43d6334f133da29ce
Developer 1 <dev1@example.internal>
2024-10-16 23:35:56 +1300

    Created a new class for storing and accessing the Recorders

---
+++ src/main/nz/ac/wgtn/swen225/lc/app/Recorders.java
@@ -0,0 +1,8 @@
+package nz.ac.wgtn.swen225.lc.app;
+
+public class Recorders{
+    private static final Recorders RECORDERS = new Recorders();
+    public static Recorders rec = RECORDERS;
+
+    private Recorders(){}
+}
