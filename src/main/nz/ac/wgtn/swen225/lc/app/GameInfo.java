029defdef9f957dbb7f0fa3f59e12e0d75d7d248
Developer 1 <dev1@example.internal>
2024-10-17 18:12:11 +1300

    Added in method to get and decrease time remaining

--- src/main/nz/ac/wgtn/swen225/lc/app/GameInfo.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/GameInfo.java
@@ -1,5 +1,13 @@
 package nz.ac.wgtn.swen225.lc.app;
 
-public record GameInfo(String levelID, int timeRemaining, int chipsRemaining){
+public class GameInfo{
+    protected String levelID;
+    protected int timeRemaining;
+    protected int chipsRemaining;
 
+    /** Decreases the amount of time remaining by one. (Which will be one second in the game.) */
+    public void decreaseTimeRemaining(){ timeRemaining--; }
+
+    /** @return The time remaining in the game. */
+    public int getTimeRemaining(){ return timeRemaining; }
 }
