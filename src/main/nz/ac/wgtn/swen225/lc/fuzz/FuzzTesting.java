3dd1648e2b5833755ec9cc6070f9c0c227e378f0
Developer 3 <dev3@example.internal>
2024-10-17 14:57:27 +1300

    added more to fuzz testing

--- src/main/nz/ac/wgtn/swen225/lc/fuzz/FuzzTesting.java
+++ src/main/nz/ac/wgtn/swen225/lc/fuzz/FuzzTesting.java
@@ -1,13 +1,9 @@
 package nz.ac.wgtn.swen225.lc.fuzz;
 import nz.ac.wgtn.swen225.lc.app.*;
-import nz.ac.wgtn.swen225.lc.domain.GameState;
 import nz.ac.wgtn.swen225.lc.domain.PlayerAction;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 
 public class FuzzTesting {
@@ -16,19 +12,28 @@ public class FuzzTesting {
   public void fuzzingTest() {
     Main.main(new String[0]);  //initialize and run main
 
-    Map<String, PlayerAction> buttonsKBM = new HashMap<>(); //variable that will store all the user keyboard inputs
-    Map<String, Runnable> buttonsGUI = new HashMap<>();     //variable that will store all the user GUI inputs
-    /*  TODO import the keyboard buttons into testing suite
+    Map<String, PlayerAction> buttonsKBM = App.strokesToPlayerAction();; //variable that will store all the user keyboard inputs
+    Map<String, Runnable> buttonsGUI = App.strokesToUIAction();     //variable that will store all the user GUI inputs
+
+    //TODO press the start button to get into the game level
+    try{
+      long startTime = System.currentTimeMillis();
+      while(System.currentTimeMillis() - startTime < 300000){       //run while loop for 5 minutes
+        Random rand = new Random();
+        List<String> keys = new ArrayList<>(buttonsKBM.keySet());
+        String randKey = keys.get(rand.nextInt(keys.size()));
+        buttonsKBM.get(randKey);
+      }
+    }catch(Exception e) {
+      throw new RuntimeException("Fuzz Testing failure, movement caused error");
+    }
+
+    /*
     *   TODO call the buttons that start the game into level 1
     *   TODO strategically spam call keyboard direction buttons to test for out of bounds checking
     *   TODO strategically spam call game buttons such as pause to test for any glitches
-    *
     */
 
-
-
   }
-
-
 
 }
