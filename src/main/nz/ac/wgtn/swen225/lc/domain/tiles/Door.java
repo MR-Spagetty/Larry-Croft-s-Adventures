c20891595e6652739d65e5c2c89d7a8fc9e71493
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2024-10-17 19:03:08 +1300

    Made TreasureDoor jsonserializable

--- src/main/nz/ac/wgtn/swen225/lc/domain/tiles/Door.java
+++ src/main/nz/ac/wgtn/swen225/lc/domain/tiles/Door.java
@@ -1,9 +1,15 @@
 package nz.ac.wgtn.swen225.lc.domain.tiles;
 
+import java.util.Optional;
+import nz.ac.wgtn.swen225.lc.domain.Colour;
 import nz.ac.wgtn.swen225.lc.domain.Point;
 import nz.ac.wgtn.swen225.lc.domain.entities.Entity;
+import nz.ac.wgtn.swen225.lc.persistency.JSONBool;
+import nz.ac.wgtn.swen225.lc.persistency.JSONObject;
+import nz.ac.wgtn.swen225.lc.persistency.JSONType;
 
-public abstract class Door extends AbstractTile {
+public abstract sealed class Door extends AbstractTile implements ModifiableTile
+    permits TreasureDoor, KeyDoor {
 
   public Door(Point location) {
     super(location);
@@ -35,7 +41,7 @@ public abstract class Door extends AbstractTile {
 
   @Override
   public final boolean canEnter(Entity enteree) {
-    return (!locked() || meetsUnlockReqs(enteree)) && super.canEnter(enteree);
+    return (meetsUnlockReqs(enteree) || !locked()) && super.canEnter(enteree);
   }
 
   @Override
@@ -45,5 +51,42 @@ public abstract class Door extends AbstractTile {
       this.locked = false;
       onUnlock(enteree);
     }
+  }
+
+  @Override
+  public JSONType toJson() {
+    JSONObject out = new JSONObject();
+    out.put("position", location().toJson());
+    out.put("locked", locked ? JSONBool.True : JSONBool.False);
+    return out;
+  }
+
+  @Override
+  public ModifiableTile fromJson(JSONType json) {
+    JSONObject data = (JSONObject) json;
+    Point pos = Point.fromJSON(data.get("position"));
+    boolean locked =
+        Optional.ofNullable(data.get("locked"))
+            .map(
+                lck -> {
+                  if (lck instanceof JSONBool) {
+                    throw new IllegalArgumentException(
+                        "Expected JSONBoolean at \"locked\" got: " + lck.getClass().getName());
+                  }
+                  return ((JSONBool) lck).get();
+                })
+            .orElse(true);
+    return switch (this) {
+      case TreasureDoor td -> {
+        Door d = new TreasureDoor(pos);
+        d.locked = locked;
+        yield d;
+      }
+      case KeyDoor kd -> {
+        Door d = new KeyDoor(pos, Colour.fromJSON(data.get("colour")));
+        d.locked = locked;
+        yield d;
+      }
+    };
   }
 }
--- src/main/nz/ac/wgtn/swen225/lc/domain/tiles/TreasureDoor.java
+++ src/main/nz/ac/wgtn/swen225/lc/domain/tiles/TreasureDoor.java
@@ -4,7 +4,7 @@ import nz.ac.wgtn.swen225.lc.domain.GameState;
 import nz.ac.wgtn.swen225.lc.domain.Point;
 import nz.ac.wgtn.swen225.lc.domain.entities.Entity;
 
-public class TreasureDoor extends Door {
+public final class TreasureDoor extends Door {
 
   public TreasureDoor(Point location) {
     super(location);
