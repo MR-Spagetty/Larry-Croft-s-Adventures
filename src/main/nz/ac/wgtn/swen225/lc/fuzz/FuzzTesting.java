edf69d5dfe3699bc061e8b095bcf0f291af1997a
Developer 3 <dev3@example.internal>
2024-10-17 21:18:45 +1300

    added TODO's and comments

--- src/main/nz/ac/wgtn/swen225/lc/fuzz/FuzzTesting.java
+++ src/main/nz/ac/wgtn/swen225/lc/fuzz/FuzzTesting.java
@@ -12,25 +12,30 @@ public class FuzzTesting {
   public void fuzzingTest() {
     Main.main(new String[0]);  //initialize and run main
 
-    Map<String, PlayerAction> buttonsKBM = App.strokesToPlayerAction();; //variable that will store all the user keyboard inputs
-    Map<String, Runnable> buttonsGUI = App.strokesToUIAction();     //variable that will store all the user GUI inputs
+    Map<String, PlayerAction> buttonsKBM = App.strokesToPlayerAction(); //variable that will store all the user keyboard inputs
+    if (buttonsKBM.isEmpty()) {throw new IllegalArgumentException("no key binds detected");}
+    Map<String, Runnable> buttonsGUI = App.strokesToUIAction();         //variable that will store all the user GUI inputs
+    if(buttonsGUI.isEmpty()) {throw new IllegalArgumentException("no GUI buttons detected");}
 
     //TODO press the start button to get into the game level
     try{
       long startTime = System.currentTimeMillis();
-      while(System.currentTimeMillis() - startTime < 300000){       //run while loop for 5 minutes
-        Random rand = new Random();
+      Random rand = new Random();
+      while(System.currentTimeMillis() - startTime < 300000){           //run while loop for 5 minutes
         List<String> keys = new ArrayList<>(buttonsKBM.keySet());
         String randKey = keys.get(rand.nextInt(keys.size()));
-        buttonsKBM.get(randKey);
+        buttonsKBM.get(randKey);  //TODO buttonsKBM.get(randKey).playerAction.run or something
       }
+      assert true;                                                      //assert true if there are no errors during fuzzing
     }catch(Exception e) {
-      throw new RuntimeException("Fuzz Testing failure, movement caused error");
+      throw new RuntimeException("Fuzz Testing failure, movement caused error: " + e.getMessage());
+    }
+    finally {
+      assert false;                                                     //assert false if error happens
     }
 
     /*
     *   TODO call the buttons that start the game into level 1
-    *   TODO strategically spam call keyboard direction buttons to test for out of bounds checking
     *   TODO strategically spam call game buttons such as pause to test for any glitches
     */
 
