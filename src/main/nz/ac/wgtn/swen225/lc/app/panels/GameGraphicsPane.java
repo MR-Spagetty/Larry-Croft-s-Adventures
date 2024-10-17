7b01c20d27ad9c26b7b37309e164e77ff56ef7ae
Developer 1 <dev1@example.internal>
2024-10-17 14:09:33 +1300

    Added in a method to get the middle of the graphics pane

--- src/main/nz/ac/wgtn/swen225/lc/app/panels/GameGraphicsPane.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/panels/GameGraphicsPane.java
@@ -4,11 +4,10 @@ import javax.swing.JPanel;
 import java.awt.*;
 
 import nz.ac.wgtn.swen225.lc.renderer.*;
+import nz.ac.wgtn.swen225.lc.domain.Point;
 
 /**
  * The part of the main screen which displays the game.
- *
- * TODO: add comments and check with Renderer that this is OK in terms of displaying the Rendered game on the UI.
  */
 public class GameGraphicsPane extends JPanel {
     Renderer r = new Renderer();
@@ -16,5 +15,10 @@ public class GameGraphicsPane extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         r.render();
+    }
+
+    /** @return The center of the Graphics pane, in the form of a Point */
+    public Point getMiddle(){
+        return new Point(0, 0);
     }
 }
