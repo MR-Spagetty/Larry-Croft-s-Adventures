1028974973fde5932c847aca3e55a5aa06350579
Developer 2 <dev2@example.internal>
2024-10-18 19:49:44 +1300

    Reconfig sound

--- src/main/nz/ac/wgtn/swen225/lc/renderer/Sound.java
+++ src/main/nz/ac/wgtn/swen225/lc/renderer/Sound.java
@@ -1,11 +1,12 @@
 package nz.ac.wgtn.swen225.lc.renderer;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
@@ -18,13 +19,18 @@ public class Sound {
    */
   public void playSound(String filename) {
     try {
-      //
+      // Get input stream from file
       AudioInputStream sound =
-          AudioSystem.getAudioInputStream(
-              new File(Path.of("src", "resources", filename + ".wav").toString()));
-      // Convert to a clip and play
-      Clip clip = AudioSystem.getClip();
+          AudioSystem.getAudioInputStream(Path.of("src", "resources", filename + ".wav").toFile());
+      // Get format
+      AudioFormat format = sound.getFormat();
+      // Setup Dataline
+      DataLine.Info info = new DataLine.Info(Clip.class, format);
+      // Convert to a clip
+      Clip clip = (Clip) AudioSystem.getLine(info);
       clip.open(sound);
+      // Play clip
+      clip.start();
     } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
       System.err.println("Sound.play():\n" + e);
     }
