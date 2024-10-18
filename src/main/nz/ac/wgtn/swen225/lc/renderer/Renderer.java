16812ee38a61b399c1e075d0b7bd2bce1fba28a2
Developer 2 <dev2@example.internal>
2024-10-18 18:52:37 +1300

    Inventory added (unaligned)

--- src/main/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
+++ src/main/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import nz.ac.wgtn.swen225.lc.app.UserInterface;
 import nz.ac.wgtn.swen225.lc.domain.GameState;
 import nz.ac.wgtn.swen225.lc.domain.Maze;
+import nz.ac.wgtn.swen225.lc.domain.Point;
 import nz.ac.wgtn.swen225.lc.domain.entities.Player;
 
 /** Handles the getting of level data and then rendering all components */
@@ -40,18 +41,12 @@ public class Renderer {
                         .getMiddle()
                         .sub(GameState.getGameState().getPlayer().location())))
         .forEach(e -> e.draw(g));
+    // used to increment item location in inventory
+    int[] slot = {0};
     // renders all inventory
-    /*int[] slot = {0};
     player.getInventory().stream()
         .filter(i -> i != null)
-        .map(
-            i ->
-                new Sprite(i) {
-                  @SuppressWarnings("unused")
-                  Point offset() {
-                    return new Point(0, 0).add(new Point(slot[0]++, 0));
-                  }
-                })
-        .forEach(i -> i.draw(g));*/
+        .map(i -> new Sprite(i, new Point(slot[0]++, 0)))
+        .forEach(i -> i.draw(g));
   }
 }
