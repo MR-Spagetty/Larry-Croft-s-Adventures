ba700bf8519a8d72c4b025cad6ac765fccfef76d
Developer 1 <dev1@example.internal>
2024-10-18 13:33:53 +1300

    Buttons are now added into the list, and then added onto the panel

--- src/main/nz/ac/wgtn/swen225/lc/app/panels/StartButtonsPanel.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/panels/StartButtonsPanel.java
@@ -12,17 +12,18 @@ import java.util.List;
  * Another custom JPanel that holds the Buttons to be displayed at the start of the game.
  */
 public class StartButtonsPanel extends JPanel {
-
-    //The list of buttons that will be on the Start menu.
-    private List<DefaultButton> startButtons = new ArrayList<>();
-
     private static final StartButtonsPanel START_BUTTONS_PANEL = new StartButtonsPanel();
     public static StartButtonsPanel sbp = START_BUTTONS_PANEL;
 
+    //The list of buttons that will be on the Start menu.
+    private final List<DefaultButton> startButtons = new ArrayList<>();
+
     private StartButtonsPanel(){
-        add(new DefaultButton(unused -> UserInterface.ui.startGame(null), "Start new game!"));
-        add(new DefaultButton(unused -> IOController.ic.resumeExistingGame(), "Resume existing game!"));
-        add(new DefaultButton(unused -> {}, "Replay a game!")); //Didn't get around to this feature :(
+        startButtons.add(new DefaultButton(unused -> UserInterface.ui.startGame(null), "Start new game!"));
+        startButtons.add(new DefaultButton(unused -> IOController.ic.resumeExistingGame(), "Resume existing game!"));
+        startButtons.add(new DefaultButton(unused -> {}, "Replay a game!")); //Didn't get around to this feature :(
+
+        startButtons.forEach(this::add);
     }
 
     /** @return The list of buttons on the Start menu. Mainly for use by Fuzz. */
