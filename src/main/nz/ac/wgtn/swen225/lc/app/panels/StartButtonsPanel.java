1cc0272270dae7589483b12d3d9c01c794f5f57f
Developer 1 <dev1@example.internal>
2024-10-18 13:21:05 +1300

    Added in a list to store the buttons used in the Start menu

--- src/main/nz/ac/wgtn/swen225/lc/app/panels/StartButtonsPanel.java
+++ src/main/nz/ac/wgtn/swen225/lc/app/panels/StartButtonsPanel.java
@@ -3,11 +3,16 @@ package nz.ac.wgtn.swen225.lc.app.panels;
 import nz.ac.wgtn.swen225.lc.app.buttons.DefaultButton;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Another custom JPanel that holds the Buttons to be displayed at the start of the game.
  */
 public class StartButtonsPanel extends JPanel {
+
+    //The list of buttons that will be on the Start menu.
+    private List<DefaultButton> startButtons = new ArrayList<>();
 
     /**
      * Constructor used when constructing the custom JPanel.
@@ -21,4 +26,7 @@ public class StartButtonsPanel extends JPanel {
         add(new DefaultButton(unused -> resumeGame.run(), "Resume existing game!"));
         add(new DefaultButton(unused -> replayGame.run(), "Replay a game!"));
     }
+
+    /** @return The list of buttons on the Start menu. Mainly for use by Fuzz. */
+    public List<DefaultButton> getStartButtons(){ return startButtons; }
 }
